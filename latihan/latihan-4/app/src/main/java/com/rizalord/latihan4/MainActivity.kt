88a0c53bb4a0fb808e400e6fddefa710e4bce52f
rizalord <ahmadkhamdani9@gmail.com>
2024-04-25 11:40:59 +0700

    feat: add latihan 4

---
+++ latihan/latihan-4/app/src/main/java/com/rizalord/latihan4/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.rizalord.latihan4
+
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var edNim : EditText
+    private lateinit var edNama: EditText
+    private lateinit var edAlamat : EditText
+    private lateinit var rgJk : RadioGroup
+    private lateinit var edTglLahir : EditText
+    private lateinit var btnClear : Button
+    private lateinit var btnSubmit : Button
+
+    private var nim: String = ""
+    private var nama: String = ""
+    private var alamat: String = ""
+    private var jk: String = ""
+    private var tglLahir: String = ""
+
+    private var year: Int = 0
+    private var month: Int = 0
+    private var day: Int = 0
+
+    private var calendar = Calendar.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Initialize the views
+        edNim = findViewById(R.id.edNim)
+        edNama = findViewById(R.id.edNama)
+        edAlamat = findViewById(R.id.edAlamat)
+        rgJk = findViewById(R.id.rgJk)
+        edTglLahir = findViewById(R.id.edTglLahir)
+        btnClear = findViewById(R.id.btnClear)
+        btnSubmit = findViewById(R.id.btnSubmit)
+
+        // Set the calendar
+        year = calendar.get(Calendar.YEAR)
+        month = calendar.get(Calendar.MONTH)
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        edTglLahir.setText("$day/$month/$year")
+        edTglLahir.setOnClickListener { showDatePicker()  }
+
+        // Clear the form
+        btnClear.setOnClickListener {
+            edNim.text.clear()
+            edNama.text.clear()
+            edAlamat.text.clear()
+            rgJk.setOnCheckedChangeListener(null)
+            rgJk.clearCheck()
+            rgJk.setOnCheckedChangeListener { _, checkedId ->
+                val radioButton = findViewById<RadioButton>(checkedId)
+                jk = radioButton.text.toString()
+            }
+            edTglLahir.text.clear()
+
+            year = calendar.get(Calendar.YEAR)
+            month = calendar.get(Calendar.MONTH)
+            day = calendar.get(Calendar.DAY_OF_MONTH)
+        }
+
+        // Radio Group
+        rgJk.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = findViewById<RadioButton>(checkedId)
+            jk = radioButton.text.toString()
+        }
+
+        // Submit the form
+        btnSubmit.setOnClickListener { submitForm() }
+
+    }
+
+    private fun showDatePicker () {
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year, monthOfYear, dayOfMonth ->
+                edTglLahir.setText("$dayOfMonth/$monthOfYear/$year")
+
+                this.year = year
+                this.month = monthOfYear
+                this.day = dayOfMonth
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
+    }
+
+    private fun submitForm () {
+        nim = edNim.text.toString()
+        nama = edNama.text.toString()
+        alamat = edAlamat.text.toString()
+        tglLahir = edTglLahir.text.toString()
+
+        // Validate the form
+        val errMessage = when {
+            nim.isEmpty() -> "NIM tidak boleh kosong"
+            nama.isEmpty() -> "Nama tidak boleh kosong"
+            alamat.isEmpty() -> "Alamat tidak boleh kosong"
+            jk.isEmpty() -> "Jenis Kelamin tidak boleh kosong"
+            tglLahir.isEmpty() -> "Tanggal Lahir tidak boleh kosong"
+            else -> ""
+        }
+
+        if (errMessage.isNotEmpty()) {
+            Toast.makeText(this, errMessage, Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Intent to the next activity
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("nim", nim)
+        intent.putExtra("nama", nama)
+        intent.putExtra("alamat", alamat)
+        intent.putExtra("jk", jk)
+        intent.putExtra("tglLahir", tglLahir)
+
+        startActivity(intent)
+    }
+}
